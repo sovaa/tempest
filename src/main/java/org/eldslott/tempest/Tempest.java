37fc104c080578886b8b27ea1da346f8524fc4b2
Oscar Eriksson <oscar.eriksson@sigma.se>
2014-09-13 19:16:37 +0800

    initial commit

---
+++ src/main/java/org/eldslott/tempest/Tempest.java
@@ -0,0 +1,27 @@
+/*
+ * This file is part of tempest.
+ *
+ * tempest is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * tempest is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with tempest.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.eldslott.tempest;
+
+/**
+ * @author <a href="mailto:oscar.eriksson@sigma.se">Oscar Eriksson</a>
+ * @date 9/13/14
+ */
+public class Tempest {
+    public static void main(String[] argv) {
+
+    }
+}
